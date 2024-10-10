00b18486a825dbad43a822ff9ec781857c9e2981
Henric Zazzi <hzazzi@kth.se>
2024-10-10 16:54:58 +0200

    deleted unnecessary files

--- web/public/js/iframe-resizer.js
+++
@@ -1,9 +0,0 @@
-function resizeIframe() {
-    var iframe = document.getElementById('mkdocs-iframe');
-    iframe.onload = function() {
-        var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-        iframe.style.height = iframeDocument.documentElement.scrollHeight + 'px';
-    };
-}
-
-window.addEventListener('load', resizeIframe);
--- web/static/mkdocs/static/js/custom.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('a').forEach(function (link) {
-        if (link.hostname !== window.location.hostname) {
-            link.target = '_blank';
-        }
-    });
-});
